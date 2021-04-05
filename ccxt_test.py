265b40165452b73feffb93dd0dd9d672a3bf946e
Atanas Delevski <adelevski@gmail.com>
2021-04-05 15:28:00 -0500

    working on fetching balances from all my exchanges. Just finished coinbase, next will do coinbasepro

--- ccxt_test.py
+++ ccxt_test.py
@@ -1,6 +1,17 @@
 import ccxt
 import config
+import json
 
+
+# Coinbase
+def coinbase_fetch(balance):
+    holding = {}
+    for data in balance['info']['data']:
+        amount = data['balance']['amount']
+        currency = data['balance']['currency']
+        if float(amount) > 0:
+            holding.update({currency: amount})
+    return holding
 
 
 coinbase = ccxt.coinbase({
@@ -14,16 +25,28 @@ coinbasepro = ccxt.coinbasepro({
 })
 binanceus = ccxt.binanceus({
     'apiKey': config.BINANCEUS_KEY,
-    'secret': config.BINANCEUS_SECRET
+    'secret': config.BINANCEUS_SECRET,
+    'nonce': ccxt.Exchange.milliseconds
 })
 kucoin = ccxt.kucoin({
     'apiKey': config.KUCOIN_KEY,
-    'secret': config.KUCOIN_SECRET
+    'secret': config.KUCOIN_SECRET,
+    'password': config.KUCOIN_PASSWORD
 })
 
 balance_coinbase = coinbase.fetch_balance()
 balance_coinbasepro = coinbasepro.fetch_balance()
 balance_binanceus = binanceus.fetch_balance()
 balance_kucoin = kucoin.fetch_balance()
+
+holding_coinbase = coinbase_fetch(balance_coinbase)
+holding_coinbasepro = {}
+holding_binanceus = {}
+holding_kucoin = {}
+
+print(holding_coinbase)
+
+
+
 
 
