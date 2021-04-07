be965466682517e0acace3e38a3caa713ac34dc2
Atanas Delevski <adelevski@gmail.com>
2021-04-07 18:11:15 -0500

    not relevant but working on BS implementation in python

---
+++ blackScholes.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy.stats import norm
+
+r = 0.01
+S = 30
+K = 40
+T = 240/365
+sigma = 0.30
+
+def blackSholes(r, S, K, T, sigma, type="C"):
+    # Calculate the BS option price for a call or put
+    d1 = (np.log(S/K) + (r + sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    try:
+        if type == "C":
+            price = S*norm.cdf(d1, 0, 1) - K*np.exp(-r*T)*norm.cdf(d2, 0, 1)
+        elif type == "P":
+            price = K*np.exp(-r*T)*norm.cdf(-d2, 0, 1) - S*norm.cdf(-d1, 0, 1)
+        return price
+    except:
+        print("Please confirm all option paramters above!!!")
+    
+print("Option Price is: ", round(blackSholes(r, S, K, T, sigma, type="P"), 2))
+
